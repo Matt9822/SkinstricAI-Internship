289b1fbd23bf49f578e072304538076066872530
Matthew <matthewrmuller@gmail.com>
2025-07-03 16:37:12 -0600

    Updated main headder to accommodate gsap

--- app/components/info.jsx
+++ app/components/info.jsx
@@ -1,14 +1,21 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import Link from "next/link";
 
 const Info = () => {
   const [name, setName] = useState(null);
   const [location, setLocation] = useState(null);
+  const [success, setSuccess] = useState(false);
+
+  useGSAP(() => {});
 
   const handleNameSubmit = (e) => {
     e.preventDefault();
     const input = e.target.elements["user-name"];
-    localStorage.setItem("Name", input.value)
+    localStorage.setItem("name", input.value);
     setName(true);
     if (input) {
       input.value = "";
@@ -18,19 +25,67 @@ const Info = () => {
   const handleLocationSubmit = (e) => {
     e.preventDefault();
     const input = e.target.elements["user-location"];
-    setLocation(input.value);
+    localStorage.setItem("location", input.value);
+    setLocation(true);
   };
 
   if (name && location) {
-    // useEffect(() => {
-      
-    // }, [])
-    console.log(localStorage)
-    // return (
-    //   <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+    const authorize = async () => {
+      try {
+        let auth = await fetch(
+            "https://us-central1-api-skinstric-ai.cloudfunctions.net/skinstricPhaseOne",
+            {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                name: localStorage.getItem("name"),
+                location: localStorage.getItem("location"),
+              }),
+            }
+          ),
+          response = await auth.json();
+        if (response.success === true) {
+          setTimeout(() => {
+            setSuccess(true);
+          }, 2000);
+        }
+      } catch (err) {
+        console.error("could not fetch info")
+      } finally {
+        console.log(success);
+      }
+    };
+    authorize();
+    if (success) {
+      return (
+        <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+          <div className="flex-col items-center gap-4 h-[72px]">
+            <div>
 
-    //   </div>
-    // );
+            <h2 className="text-2xl font-normal text-[#1A1B1C]">Thank you!</h2>
+            </div>
+            <div>
+
+            <p>Proceed for the next step</p>
+            </div>
+          </div>
+          <div className="absolute">
+            <Link rel="stylesheet" href="/analysis">
+              <button>
+                <p className="uppercase">proceed</p>
+                <Image width={56} height={56} src="/button.svg" alt="Button" />
+              </button>
+            </Link>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+          <p>Processing submission</p>
+        </div>
+      );
+    }
   } else if (name && !location) {
     return (
       <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,20 @@
+"use client";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
-import Anibutton from "./components/button";
 import Link from "next/link";
 
 export default function Home() {
+  useGSAP(() => {
+    gsap.to("#main-header", {x: 0, })
+  });
   return (
     <div className="">
       <main className="flex items-center justify-between">
         <div className="flex">
+          <div className="fixed w-[420px] h-[420px] rotate-45 -translate-x-1/2 left-[2px] top-[297px] cursor-pointer z-1 border-2 border-dashed max-[1020]:hidden"></div>
           <div className="absolute top-[51%] left-[30px]">
-            <button className="flex items-center">
+            <button className="flex items-center cursor-pointer">
               <Image
                 src="/button.svg"
                 width={44}
@@ -21,22 +27,14 @@ export default function Home() {
               </p>
             </button>
           </div>
-          <div className="flex">
-            <Image
-              src="/rectangleSquare.svg"
-              width={301}
-              height={602}
-              alt="dotted line"
-              className="max-[1020px]:hidden"
-            />
-          </div>
+          <div className="flex w-[300] max-[1020px]:hidden"></div>
         </div>
         <div className="flex items-center w-[593.97px] text-center h-[93vh]">
           <div className="max-[1020px]:relative flex items-center justify-center w-full h-full">
             <div className="border-2 border-dotted w-[420px] h-[420] fixed rotate-45 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 min-[1020px]:hidden" />
             <h1
               id="main-header"
-              className="text-[98px]/[120px] font-[300] top-1/2 max-[1020px]:text-[60px]/[1] max-[1020px]:absolute max-[1020px]:-translate-y-full max-[1020px]:w-full"
+              className="min-[1020px]:fixed min-[1020px]:w-[800px] min-[1020px]:-translate-y-1/2 text-[98px]/[100px] font-[300] min-[1020px]:top-[500px] max-[1020px]:text-[60px]/[1] max-[1020px]:absolute max-[1020px]:-translate-y-full max-[1020px]:w-full max-[1020px]:top-1/2"
             >
               Sophisticated skincare
             </h1>
@@ -65,7 +63,30 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <Anibutton />
+        <div className="flex">
+          <Link
+            rel="stylesheet"
+            href="/test"
+            className="fixed rotate-45 w-[420px] h-[420px] -translate-x-1/2 right-[-420px] top-[297px] "
+          >
+            <div className="fixed w-[420px] h-[420px] cursor-pointer z-1 border-2 border-dashed max-[1020]:hidden"></div>
+          </Link>
+          <div className="absolute top-[51%] right-[30px]">
+            <button className="flex items-center cursor-pointer">
+              <p className="text-[14px]/[16px] pr-2 max-[1020px]:hidden">
+                TAKE TEST
+              </p>
+              <Image
+                src="/button.svg"
+                width={44}
+                height={44}
+                alt="button"
+                className="max-[1020px]:hidden"
+              />
+            </button>
+          </div>
+          <div className="flex w-[300] max-[1020px]:hidden"></div>
+        </div>
       </main>
     </div>
   );
