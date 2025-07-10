3ac9f3f761df2133525aa852126edd2504086c34
Matthew <matthewrmuller@gmail.com>
2025-07-10 01:50:55 -0600

    So close

--- app/analysis/page.jsx
+++ app/analysis/page.jsx
@@ -2,7 +2,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React, { useEffect, useState, useRef } from "react";
-import Demographics from "../components/demographics"
+import Demographics from "../components/demographics";
 import HandleSelection from "../components/handleSelection";
 
 export default function Analysis() {
@@ -11,7 +11,10 @@ export default function Analysis() {
   const [race, setRace] = useState(null);
   const [demo, setDemo] = useState(false);
   const [selected, setSelected] = useState("race");
-  
+  const [raceSelect, setRaceSelect] = useState("");
+  const [ageSelect, setAgeSelect] = useState("");
+  const [sexSelect, setSexSelect] = useState("");
+  const hasSetInitialRace = useRef(false);
 
   useEffect(() => {
     const getData = JSON.parse(localStorage.getItem("image"));
@@ -24,25 +27,33 @@ export default function Analysis() {
 
   const getSortedAgeGroup = () => {
     if (!age || typeof age !== "object") return null;
-      const sorted = Object.entries(age).sort((a, b) => b[1] - a[1]);
-      return sorted;
+    const sorted = Object.entries(age).sort((a, b) => b[1] - a[1]);
+    return sorted;
   };
   const getSortedRaceGroup = () => {
     if (!race || typeof race !== "object") return null;
-      const sorted = Object.entries(race).sort((a, b) => b[1] - a[1]);
-      return sorted;
+    const sorted = Object.entries(race).sort((a, b) => b[1] - a[1]);
+    return sorted;
   };
   const getSortedGenderGroup = () => {
     if (!gender || typeof gender !== "object") return null;
-      const sorted = Object.entries(gender).sort((a, b) => b[1] - a[1]);
-      return sorted;
-    };
-    
-    const sortedAgeGroup = getSortedAgeGroup()
-    const sortedRaceGroup = getSortedRaceGroup()
-    const sortedGenderGroup = getSortedGenderGroup()
-    
-    console.log(sortedRaceGroup)
+    const sorted = Object.entries(gender).sort((a, b) => b[1] - a[1]);
+    return sorted;
+  };
+
+  const sortedAgeGroup = getSortedAgeGroup();
+  const sortedRaceGroup = getSortedRaceGroup();
+  const sortedGenderGroup = getSortedGenderGroup();
+
+  useEffect(() => {
+    if (!hasSetInitialRace.current && sortedRaceGroup && sortedRaceGroup[0]) {
+      setRaceSelect(sortedRaceGroup[0][0]);
+      setAgeSelect(sortedAgeGroup[0][0]);
+      setSexSelect(sortedGenderGroup[0][0]);
+      hasSetInitialRace.current = true;
+    }
+  }, [sortedRaceGroup, sortedAgeGroup, sortedGenderGroup]);
+
   if (!demo) {
     return (
       <div className="flex-col h-[93vh] w-full">
@@ -53,34 +64,58 @@ export default function Analysis() {
         </div>
         <div className="flex justify-between w-full ">
           <ul className="flex-col ml-10 w-[400]">
-            <li onClick={() => setSelected("race")} className={`${selected === "race" ? "bg-black text-white" : "bg-gray-100"} flex items-center py-8 px-2 border-t-1 cursor-pointer ease-in-out duration-400`}>
+            <li
+              onClick={() => setSelected("race")}
+              className={`${
+                selected === "race" ? "bg-black text-white" : "bg-gray-100"
+              } flex items-center py-8 px-2 border-t-1 cursor-pointer ease-in-out duration-400`}
+            >
               <p className="mr-1 font-[600] text-[15px] uppercase">race:</p>
-              {sortedRaceGroup ? <p>{sortedRaceGroup[0][0]}</p> : <p>N/A</p>}
+              {raceSelect ? <p>{raceSelect}</p> : <p>N/A</p>}
             </li>
-            <li onClick={() => setSelected("age")} className={`${selected === "age" ? "bg-black text-white" : "bg-gray-100"} flex items-center my-5 py-8 px-2 border-t-1 cursor-pointer ease-in-out duration-400`}>
+            <li
+              onClick={() => setSelected("age")}
+              className={`${
+                selected === "age" ? "bg-black text-white" : "bg-gray-100"
+              } flex items-center my-5 py-8 px-2 border-t-1 cursor-pointer ease-in-out duration-400`}
+            >
               <p className="mr-1 font-[600] text-[15px] uppercase">age:</p>
-              {sortedAgeGroup ? <p>{sortedAgeGroup[0][0]}</p> : <p>N/A</p>}
+              {ageSelect ? <p>{ageSelect}</p> : <p>N/A</p>}
             </li>
-            <li onClick={() => setSelected("gender")} className={`${selected === "gender" ? "bg-black text-white" : "bg-gray-100"} flex items-center py-8 px-2 border-t-1 cursor-pointer ease-in-out duration-400`}>
+            <li
+              onClick={() => setSelected("gender")}
+              className={`${
+                selected === "gender" ? "bg-black text-white" : "bg-gray-100"
+              } flex items-center py-8 px-2 border-t-1 cursor-pointer ease-in-out duration-400`}
+            >
               <p className="mr-1 font-[600] text-[15px] uppercase">sex:</p>
-              {sortedGenderGroup ? <p>{sortedGenderGroup[0][0]}</p> : <p>N/A</p>}
+              {sexSelect ? <p>{sexSelect}</p> : <p>N/A</p>}
             </li>
           </ul>
-          <div className="flex bg-gray-100 mx-10 w-full">
+          <div className="flex bg-gray-100 mx-10 w-full border-t-1">
             <div>
               <p>center</p>
             </div>
             <div></div>
           </div>
-          <div className="flex-col mr-10 bg-gray-100 w-[450]">
-            <HandleSelection sortedAge={sortedAgeGroup} sortedRace={sortedRaceGroup} sortedGender={sortedGenderGroup} setSelect={selected}/>
+          <div className="flex-col mr-10 pb-5 pt-3 border-t-1 bg-gray-100 w-[600]">
+            <HandleSelection
+              setSexSelect={setSexSelect}
+              setAgeSelect={setAgeSelect}
+              setRaceSelect={setRaceSelect}
+              sexSelect={sexSelect}
+              ageSelect={ageSelect}
+              raceSelect={raceSelect}
+              sortedAge={sortedAgeGroup}
+              sortedRace={sortedRaceGroup}
+              sortedGender={sortedGenderGroup}
+              setSelect={selected}
+            />
           </div>
         </div>
       </div>
     );
   } else {
-    return (
-      <Demographics ready={setDemo}/>
-    );
+    return <Demographics ready={setDemo} />;
   }
 }
--- app/components/handleSelection.jsx
+++ app/components/handleSelection.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const handleSelection = ({
@@ -5,24 +6,127 @@ const handleSelection = ({
   sortedRace,
   sortedGender,
   setSelect,
+  raceSelect,
+  setRaceSelect,
+  ageSelect,
+  setAgeSelect,
+  sexSelect,
+  setSexSelect,
 }) => {
   if (setSelect === "race") {
     return (
-      <ul>
-        {sortedRace
-          ? sortedRace.map((item, index) => (
-              <li key={index} className="flex">
-                <p>{item[0]}</p>
-                <p>{item[1]}</p>
-              </li>
-            ))
-          : ""}
-      </ul>
+      <div>
+        <div className="flex justify-between px-2 pb-2">
+          <p className="uppercase text-[16px]">race</p>
+          <p className="uppercase text-[16px]">a.i. confidence</p>
+        </div>
+        <ul>
+          {sortedRace
+            ? sortedRace.map((item, index) => (
+                <li
+                  key={index}
+                  onClick={() => setRaceSelect(`${item[0]}`)}
+                  className={`flex justify-between ${
+                    raceSelect === item[0]
+                      ? "bg-black text-white"
+                      : "bg-gray-100 cursor-pointer"
+                  } ease-in-out duration-400 p-3 w-full`}
+                >
+                  <p className="cursor-pointer flex">
+                    <Image
+                      src="diamonds.svg"
+                      alt=">"
+                      width={15}
+                      height={15}
+                      className={`flex justify-between ${
+                        raceSelect === item[0] ? "invert" : ""
+                      } ease-in-out duration-400 mr-3`}
+                    />
+                    {item[0]}
+                  </p>
+                  <p className="cursor-pointer">{Math.round(item[1] * 100)}%</p>
+                </li>
+              ))
+            : "Error try again"}
+        </ul>
+      </div>
     );
   } else if (setSelect === "age") {
-    return <div>age</div>;
+    return (
+      <div>
+        <div className="flex justify-between px-2 pb-2">
+          <p className="uppercase text-[16px]">AGE</p>
+          <p className="uppercase text-[16px]">a.i. confidence</p>
+        </div>
+        <ul>
+          {sortedAge
+            ? sortedAge.map((item, index) => (
+                <li
+                  key={index}
+                  onClick={() => setAgeSelect(`${item[0]}`)}
+                  className={`flex justify-between items-center ${
+                    ageSelect === item[0]
+                      ? "bg-black text-white"
+                      : "bg-gray-100 cursor-pointer"
+                  } ease-in-out duration-400 p-3`}
+                >
+                  <p className="cursor-pointer flex">
+                    <Image
+                      src="diamonds.svg"
+                      alt=">"
+                      width={15}
+                      height={15}
+                      className={`${
+                        ageSelect === item[0] ? "invert" : ""
+                      } ease-in-out duration-400 mr-3`}
+                    />
+                    {item[0]}
+                  </p>
+                  <p className="cursor-pointer">{Math.round(item[1] * 100)}%</p>
+                </li>
+              ))
+            : "Error try again"}
+        </ul>
+      </div>
+    );
   } else if (setSelect === "gender") {
-    return <div>sex</div>;
+    return (
+      <div>
+        <div className="flex justify-between px-2 pb-2">
+          <p className="uppercase text-[16px]">Sex</p>
+          <p className="uppercase text-[16px]">a.i. confidence</p>
+        </div>
+        <ul>
+          {sortedGender
+            ? sortedGender.map((item, index) => (
+                <li
+                  key={index}
+                  onClick={() => setSexSelect(`${item[0]}`)}
+                  className={`flex justify-between ${
+                    sexSelect === item[0]
+                      ? "bg-black text-white"
+                      : "bg-gray-100 cursor-pointer"
+                  } ease-in-out duration-400 p-3`}
+                >
+                  <p className="cursor-pointer flex">
+                    <Image
+                      src="diamonds.svg"
+                      alt=">"
+                      width={15}
+                      height={15}
+                      className={`flex justify-between ${
+                        sexSelect === item[0] ? "invert" : ""
+                      } ease-in-out duration-400 mr-3`}
+                    />
+                    {item[0]}
+                  </p>
+                  <p className="cursor-pointer">{Math.round(item[1] * 100)}%</p>
+                </li>
+              ))
+            : "Error try again"}
+        </ul>
+      </div>
+    );
   }
 };
 
